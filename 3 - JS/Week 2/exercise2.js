0c9cd2a8815e8c5dc7a4e923e0c90bd98ef71309
Ghefar-elbasha <ghefar_elbasha@learners.purplebeard.co.uk>
2023-05-04 00:30:55 +0100

    saved all

---
+++ 3 - JS/Week 2/exercise2.js
@@ -0,0 +1,8 @@
+function getRandomNum(1, 10) {
+    return Math.random() * (1 - 10) + 1;
+}{
+if (number too high);}{
+    return "too high" }{
+    else if ( number too low )}{
+    return"too low"}
+    
