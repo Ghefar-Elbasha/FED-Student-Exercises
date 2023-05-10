a6f72340aa6a35faa388ea6c8a2cbffa42532fbf
Ghefar-elbasha <ghefar_elbasha@learners.purplebeard.co.uk>
2023-05-10 23:04:13 +0100

    saved all

---
+++ 3 - JS/Week 2/exercise3.js
@@ -0,0 +1,8 @@
+
+const number = prompt("Enter a number:")
+    if (number / 3) 
+      console.log=("Fizz");
+    else if (number / 5)
+    console.log("Buzz") 
+    else
+      console.log("FizzBuzz");
---
+++ 3 - JS/Week 3/exercise1.js
@@ -0,0 +1,5 @@
+// Create a function that checks if a word is a palindrome.
+
+// 💡 Hint: A palindrome is a word that is spelled the same way forwards and backwad 
+
+// You should use `.addEventListenter()` to listen for a word from a form 
